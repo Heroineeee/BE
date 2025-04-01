2069ff4492a25ca97f6cfe255494bd779b9f94b9
chaen-ing <kimce0405@naver.com>
2025-04-01 23:49:57 +0900

    Debug : 구글, 네이버로 로그인 요청시 예외 (#3)

--- src/main/java/com/utopia/utopia_be/auth/service/strategy/NaverLoginStrategy.java
+++ src/main/java/com/utopia/utopia_be/auth/service/strategy/NaverLoginStrategy.java
@@ -1,14 +1,17 @@
 package com.utopia.utopia_be.auth.service.strategy;
 
+import static com.utopia.utopia_be.auth.exception.errorcode.AuthErrorCode.*;
+
 import org.springframework.stereotype.Service;
 
 import com.utopia.utopia_be.auth.dto.response.LoginResponse;
+import com.utopia.utopia_be.auth.exception.AuthException;
 
 @Service
 public class NaverLoginStrategy implements SocialLoginStrategy {
 
   @Override
   public LoginResponse login(String code) {
-    return null;
+    throw new AuthException(LOGIN_TYPE_NOT_SUPPORTED);
   }
 }
