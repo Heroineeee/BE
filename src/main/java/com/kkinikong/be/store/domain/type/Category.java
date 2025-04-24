4b47114ce7d1189d6dc8d372cc1dcc47f24ad7ca
erika0915 <soohee2001@gmail.com>
2025-04-24 17:14:49 +0900

    Refactor : 카테고리 enum에 label 필드 추가 (#7)

--- src/main/java/com/kkinikong/be/store/domain/type/Category.java
+++ src/main/java/com/kkinikong/be/store/domain/type/Category.java
@@ -1,15 +1,25 @@
 package com.kkinikong.be.store.domain.type;
 
 public enum Category {
-  한식,
-  양식,
-  일식,
-  중식,
-  치킨,
-  분식,
-  샤브샤브,
-  아시아음식,
-  도시락,
-  간식,
-  기타;
+  KOREAN("한식"),
+  WESTERN("양식"),
+  JAPANESE("일식"),
+  CHINESE("중식"),
+  CHICKEN("치킨"),
+  BUNSIK("분식"),
+  SHABU("샤브샤브"),
+  ASIAN("아시아음식"),
+  LUNCHBOX("도시락"),
+  DESSERT("간식"),
+  ETC("기타");
+
+  private final String label;
+
+  Category(String label) {
+    this.label = label;
+  }
+
+  public String getLabel() {
+    return label;
+  }
 }
--- src/main/java/com/kkinikong/be/store/domain/type/TagCategory.java
+++ src/main/java/com/kkinikong/be/store/domain/type/TagCategory.java
@@ -1,8 +1,18 @@
 package com.kkinikong.be.store.domain.type;
 
 public enum TagCategory {
-  메뉴,
-  공간,
-  결제,
-  기타
+  MENU("메뉴"),
+  SPACE("공간"),
+  PAYMENT("결제"),
+  ETC("기타");
+
+  private final String label;
+
+  TagCategory(String label) {
+    this.label = label;
+  }
+
+  public String getLabel() {
+    return label;
+  }
 }
