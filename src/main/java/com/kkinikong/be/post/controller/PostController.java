06553201d4e431d2d1aedd2f565af111d2f00353
chaen-ing <kimce0405@naver.com>
2025-05-04 15:49:08 +0900

    SCRUM-19 Feat : 테스트 커밋

--- src/main/java/com/kkinikong/be/post/controller/PostController.java
+++ src/main/java/com/kkinikong/be/post/controller/PostController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 import com.kkinikong.be.global.response.ApiResponse;
@@ -13,6 +14,7 @@ import com.kkinikong.be.global.response.ApiResponse;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/post")
+@Tag(name = "Post", description = "Post 관련 API")
 public class PostController {
 
   @GetMapping("/test")
