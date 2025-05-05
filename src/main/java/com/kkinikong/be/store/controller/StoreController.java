621a5927d7fa4674fc54eb3640c860f2bd203c6d
chaen-ing <kimce0405@naver.com>
2025-05-06 02:12:37 +0900

    SCRUM-8 Feat : Store 컨트롤러 생성

--- src/main/java/com/kkinikong/be/store/controller/StoreController.java
+++ src/main/java/com/kkinikong/be/store/controller/StoreController.java
@@ -1,3 +1,42 @@
 package com.kkinikong.be.store.controller;
 
-public class StoreController {}
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+import com.kkinikong.be.global.response.ApiResponse;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/store")
+@Tag(name = "Store", description = "가맹점 관련 API")
+@RestController
+public class StoreController {
+
+  @Operation(summary = "가맹점 상세 정보 조회", description = "가맹점 상세 정보를 조회합니다.")
+  @GetMapping("/{storeId}")
+  public ResponseEntity<ApiResponse<Object>> getStoreInfo(@PathVariable("storeId") Long storeId) {
+    return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.EMPTY_RESPONSE);
+  }
+
+  @Operation(summary = "가맹점 메뉴 보러가기 및 길찾기", description = "가맹점 메뉴 보러가기 및 길찾기 링크를 제공합니다.")
+  @GetMapping("/{storeId}")
+  public ResponseEntity<ApiResponse<Object>> getStoreExternalLink(
+      @PathVariable("storeId") Long storeId) {
+    return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.EMPTY_RESPONSE);
+  }
+
+  @Operation(summary = "가맹점 정보 수정 요청", description = "가맹점 정보 수정 요청을 합니다.")
+  @PostMapping("/{storeId}/report")
+  public ResponseEntity<ApiResponse<Object>> postStoreReport(
+      @PathVariable("storeId") Long storeId) {
+    return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.EMPTY_RESPONSE);
+  }
+}
