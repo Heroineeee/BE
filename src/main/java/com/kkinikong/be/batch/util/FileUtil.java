02a68c9bcb30aa488d5e8f6307dc0726352d0058
erika0915 <soohee2001@gmail.com>
2025-05-05 17:47:39 +0900

    Refactor : FileUtil SHA-256 해시 생성 시 2자리 hex 포맷 보장 및 커스텀 예외 적용 (#7)

--- src/main/java/com/kkinikong/be/batch/util/FileUtil.java
+++ src/main/java/com/kkinikong/be/batch/util/FileUtil.java
@@ -2,33 +2,38 @@ package com.kkinikong.be.batch.util;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import com.kkinikong.be.batch.exception.BatchException;
+import com.kkinikong.be.batch.exception.errorcode.BatchErrorCode;
 
 public class FileUtil {
 
-  // 파일의 SHA-256 해시값을 계산하는 함수
+  // 파일의 SHA-256 해시값을 계산
   public static String getFileHash(String filePath) {
-    try {
+
+    try (FileInputStream fis = new FileInputStream(new File(filePath))) {
       MessageDigest digest = MessageDigest.getInstance("SHA-256");
       FileInputStream fileInputStream = new FileInputStream(new File(filePath));
-      byte[] byteArray = new byte[1024];
+      byte[] buffer = new byte[1024];
       int bytesRead;
 
-      while ((bytesRead = fileInputStream.read(byteArray)) != -1) {
-        digest.update(byteArray, 0, bytesRead);
+      while ((bytesRead = fis.read(buffer)) != -1) {
+        digest.update(buffer, 0, bytesRead);
       }
 
       byte[] hashBytes = digest.digest();
       StringBuilder hexString = new StringBuilder();
 
       for (byte b : hashBytes) {
-        hexString.append(Integer.toHexString(0xFF & b));
+        hexString.append(String.format("%02x", b));
       }
 
-      return hexString.toString(); // SHA-256 해시값 반환
-    } catch (Exception e) {
-      e.printStackTrace();
+      return hexString.toString();
+    } catch (IOException | NoSuchAlgorithmException e) {
+      throw new BatchException(BatchErrorCode.FILE_HASH_FAILED);
     }
-    return null;
   }
 }
