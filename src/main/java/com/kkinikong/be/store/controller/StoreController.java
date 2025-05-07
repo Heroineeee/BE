6b60672d5e7f44a82fa7ebf165c97937f54346a5
erika0915 <soohee2001@gmail.com>
2025-05-07 21:06:10 +0900

    [SCRUM-43] Chore : swagger 설명 수정

--- src/main/java/com/kkinikong/be/store/controller/StoreController.java
+++ src/main/java/com/kkinikong/be/store/controller/StoreController.java
@@ -29,12 +29,13 @@ public class StoreController {
       summary = "가맹점 찾기 리스트 조회",
       description =
           """
-        현재 위치 기반으로 가맹점 목록을 조회합니다. 페이지 번호는 0부터 시작하며, 페이지 당 10개의 가맹점을 반환합니다.
+        현재 위치 기반으로 가맹점 목록을 조회합니다. 페이지 번호는 0부터 시작합니다.
         - category를 선택하지 않으면 전체 가맹점 조회
         - 정렬 조건은 가까운 순(DISTANCE), 별점 높은 순(RATING), 리뷰 많은 순(REVIEW_COUNT), 조회수 순(VIEW_COUNT) 중 선택
         - 필터링 결과가 동일할 경우, 이름 가나다순으로 정렬
         - 로그인한 유저는 isScrapped 가 true/false 로 반환
         - 로그인하지 않은 경우, isScrapped는 null로 응답
+        - 페이지 크기(size)는 기본 10개이며, 조정 가능
         """)
   @GetMapping
   public ResponseEntity<ApiResponse<Object>> getStoresList(
