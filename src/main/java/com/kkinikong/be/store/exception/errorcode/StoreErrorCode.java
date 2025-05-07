9b48865ff866234b2599a46b0311f15d5f13b70e
chaen-ing <kimce0405@naver.com>
2025-05-07 16:47:54 +0900

    SCRUM-8 Feat : 에러코드 추가

--- src/main/java/com/kkinikong/be/store/exception/errorcode/StoreErrorCode.java
+++ src/main/java/com/kkinikong/be/store/exception/errorcode/StoreErrorCode.java
@@ -10,6 +10,7 @@ import com.kkinikong.be.global.exception.errorcode.ErrorCode;
 @Getter
 @RequiredArgsConstructor
 public enum StoreErrorCode implements ErrorCode {
+  STORE_NOT_FOUND(HttpStatus.BAD_REQUEST, "가맹점을 찾을 수 없습니다."),
   ;
   private final HttpStatus httpStatus;
   private final String message;
