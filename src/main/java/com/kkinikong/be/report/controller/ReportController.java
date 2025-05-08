1b4c8bc4be7d6127c3f095b23f59d3a4273818c7
chaen-ing <kimce0405@naver.com>
2025-05-08 23:31:11 +0900

    SCRUM-8 Feat : 가맹점 정보 수정 요청 컨트롤러, 서비스 생성

--- src/main/java/com/kkinikong/be/report/controller/ReportController.java
+++ src/main/java/com/kkinikong/be/report/controller/ReportController.java
@@ -1,3 +1,27 @@
 package com.kkinikong.be.report.controller;
 
-public class ReportController {}
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+import com.kkinikong.be.global.response.ApiResponse;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/report")
+@Tag(name = "Report", description = "신고 관련 API")
+@RestController
+public class ReportController {
+
+  @Operation(summary = "가맹점 정보 수정 요청", description = "가맹점 정보 수정 요청을 합니다.")
+  @PostMapping("/{storeId}")
+  public ResponseEntity<ApiResponse<Object>> reportStore(@PathVariable("storeId") Long storeId) {
+    return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.EMPTY_RESPONSE);
+  }
+}
--- src/main/java/com/kkinikong/be/report/service/ReportService.java
+++ src/main/java/com/kkinikong/be/report/service/ReportService.java
@@ -1,3 +1,13 @@
 package com.kkinikong.be.report.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
 public class ReportService {}
