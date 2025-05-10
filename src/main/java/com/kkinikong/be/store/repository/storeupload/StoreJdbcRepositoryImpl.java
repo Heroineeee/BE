8dc768fd1fa3f7532d7ca4292b8657c2a5787535
erika0915 <soohee2001@gmail.com>
2025-05-10 17:13:56 +0900

    [SCRUM-54] Fix : inset 쿼리에 파라미터 개수 오류 수정

--- src/main/java/com/kkinikong/be/store/repository/storeupload/StoreJdbcRepositoryImpl.java
+++ src/main/java/com/kkinikong/be/store/repository/storeupload/StoreJdbcRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.kkinikong.be.store.repository.storeupload;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -18,19 +19,21 @@ public class StoreJdbcRepositoryImpl implements StoreJdbcRepository {
   private final JdbcTemplate jdbcTemplate;
   private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
+  private static final int BATCH_SIZE = 1000;
+
   /// 새로운 가맹점 리스트를 Batch Insert
   @Override
   @Transactional
   public void saveAllByJdbcTemplate(List<Store> stores) {
     String sql =
         "INSERT INTO stores "
-            + "(name, region, category, address, latitude, longitude, rating_avg, scrap_count, review_count, view_count, updated_date) "
-            + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            + "(name, region, category, address, latitude, longitude, rating_avg, scrap_count, review_count, view_count, updated_date, created_date, modified_date) "
+            + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"; // ★ created_date 추가
 
     jdbcTemplate.batchUpdate(
         sql,
         stores,
-        1000,
+        BATCH_SIZE,
         (ps, store) -> {
           ps.setString(1, store.getName());
           ps.setString(2, store.getRegion());
@@ -38,11 +41,13 @@ public class StoreJdbcRepositoryImpl implements StoreJdbcRepository {
           ps.setString(4, store.getAddress());
           ps.setDouble(5, store.getLatitude());
           ps.setDouble(6, store.getLongitude());
-          ps.setDouble(7, 0.0); // 초기값 ratingAvg
-          ps.setLong(8, 0L); // 초기값 scrapCount
-          ps.setLong(9, 0L); // 초기값 reviewCount
-          ps.setLong(10, 0L); // 초기값 viewCount
+          ps.setDouble(7, 0.0);
+          ps.setLong(8, 0L);
+          ps.setLong(9, 0L);
+          ps.setLong(10, 0L);
           ps.setObject(11, store.getUpdatedDate());
+          ps.setObject(12, LocalDate.now());
+          ps.setObject(13, LocalDate.now());
         });
   }
 
