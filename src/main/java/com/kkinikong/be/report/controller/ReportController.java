ada8cf23a4a06b02b9ec4eb85f90aa3ff269d0a2
chaen-ing <kimce0405@naver.com>
2025-05-11 03:26:05 +0900

    [SCRUM-10] Debug : 오타수정

--- src/main/java/com/kkinikong/be/report/controller/ReportController.java
+++ src/main/java/com/kkinikong/be/report/controller/ReportController.java
@@ -62,7 +62,7 @@ public class ReportController {
       - ETC를 선택한 경우, description을 입력해주세요.
       - description은 500자 이내로 작성해주세요.
       - ETC를 선택하지 않은 경우, description은 내용이 있더라도 null로 전달되며 requestBody를 비워서 보내도 됩니다.
-      - 이미 신고한 가게인 경우, REPORT_ALREAY_EXISTS 400 에러를 반환합니다.
+      - 이미 신고한 가게인 경우, REPORT_ALREADY_EXISTS 400 에러를 반환합니다.
       """)
   @PostMapping("review/{reviewId}")
   public ResponseEntity<ApiResponse<Object>> reportReview(
