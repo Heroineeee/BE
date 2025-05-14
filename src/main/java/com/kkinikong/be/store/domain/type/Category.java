70002fa55030b4ac47cd91d6b6f283edaf61eb60
erika0915 <soohee2001@gmail.com>
2025-05-14 20:50:50 +0900

    [SCRUM-44] Chore : 필요 없는 코드 삭제

--- src/main/java/com/kkinikong/be/store/domain/type/Category.java
+++ src/main/java/com/kkinikong/be/store/domain/type/Category.java
@@ -21,13 +21,4 @@ public enum Category {
   Category(String label) {
     this.label = label;
   }
-
-  public static Category fromLabel(String label) {
-    for (Category category : Category.values()) {
-      if (category.getLabel().equals(label)) {
-        return category;
-      }
-    }
-    return null;
-  }
 }
