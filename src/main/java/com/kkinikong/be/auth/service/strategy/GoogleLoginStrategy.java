928770a5c7198c45a7cdfea3c7470a0b1a1efcf4
erika0915 <soohee2001@gmail.com>
2025-05-19 14:42:55 +0900

    [SCRUM-79] Feat : 구글 소셜 로그인 구현

--- src/main/java/com/kkinikong/be/auth/service/strategy/GoogleLoginStrategy.java
+++ src/main/java/com/kkinikong/be/auth/service/strategy/GoogleLoginStrategy.java
@@ -4,14 +4,30 @@ import static com.kkinikong.be.auth.exception.errorcode.AuthErrorCode.*;
 
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
 import com.kkinikong.be.auth.dto.response.LoginResponse;
-import com.kkinikong.be.auth.exception.AuthException;
+import com.kkinikong.be.auth.dto.response.google.GoogleUserInfoResponse;
+import com.kkinikong.be.auth.util.JwtTokenProvider;
+import com.kkinikong.be.auth.util.google.GoogleApiClient;
+import com.kkinikong.be.user.domain.User;
+import com.kkinikong.be.user.domain.type.LoginType;
+import com.kkinikong.be.user.service.UserService;
 
 @Service("GOOGLE")
+@RequiredArgsConstructor
 public class GoogleLoginStrategy implements SocialLoginStrategy {
+
+  private final GoogleApiClient googleApiClient;
+  private final UserService userService;
+  private final JwtTokenProvider jwtTokenProvider;
 
   @Override
   public LoginResponse login(String code) {
-    throw new AuthException(LOGIN_TYPE_NOT_SUPPORTED);
+    String accessToken = googleApiClient.getAccessToken(code);
+    GoogleUserInfoResponse userInfo = googleApiClient.getUserInfo(accessToken);
+    User user = userService.findOrCreateUser(userInfo.email(), LoginType.GOOGLE);
+    String token = jwtTokenProvider.createToken(user.getId().toString());
+    return LoginResponse.from(user.getNickname(), user.getRole(), token);
   }
 }
---
+++ src/main/java/com/kkinikong/be/auth/util/google/GoogleApiClient.java
@@ -0,0 +1,58 @@
+package com.kkinikong.be.auth.util.google;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import lombok.RequiredArgsConstructor;
+
+import com.kkinikong.be.auth.dto.response.google.GoogleLoginResponse;
+import com.kkinikong.be.auth.dto.response.google.GoogleUserInfoResponse;
+
+@RequiredArgsConstructor
+@Component
+public class GoogleApiClient {
+  private final WebClient webClient;
+  private static final String TOKEN_REQUEST_URI = "https://oauth2.googleapis.com/token";
+  private static final String USER_INFO_URI = "https://www.googleapis.com/oauth2/v1/userinfo";
+
+  @Value("${VITE_GOOGLE_CLIENT_ID}")
+  private String googleApiKey;
+
+  @Value("${VITE_GOOGLE_REDIRECT_URI}")
+  private String googleRedirectUri;
+
+  public String getAccessToken(String code) {
+    return webClient
+        .post()
+        .uri(TOKEN_REQUEST_URI)
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+        .bodyValue(
+            "grant_type=authorization_code"
+                + "&client_id="
+                + googleApiKey
+                + "&redirect_uri="
+                + googleRedirectUri
+                + "&code="
+                + code)
+        .retrieve()
+        .bodyToMono(GoogleLoginResponse.class)
+        .map(GoogleLoginResponse::accessToken)
+        .block();
+  }
+
+  public GoogleUserInfoResponse getUserInfo(String accessToken) {
+    return webClient
+        .get()
+        .uri(USER_INFO_URI)
+        .headers(
+            headers -> {
+              headers.setBearerAuth(accessToken);
+              headers.setContentType(MediaType.APPLICATION_JSON);
+            })
+        .retrieve()
+        .bodyToMono(GoogleUserInfoResponse.class)
+        .block();
+  }
+}
