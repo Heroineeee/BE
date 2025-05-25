e795f745e748dd296f0f5674d5dc4c1d31a36310
erika0915 <soohee2001@gmail.com>
2025-05-26 01:26:01 +0900

    [SCRUM-102] Feat : dto 작성

--- src/main/java/com/kkinikong/be/user/controller/UserController.java
+++ src/main/java/com/kkinikong/be/user/controller/UserController.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 
 import com.kkinikong.be.global.response.ApiResponse;
 import com.kkinikong.be.user.dto.request.NicknameRequest;
+import com.kkinikong.be.user.dto.request.UserPlaceRequest;
 import com.kkinikong.be.user.dto.response.NicknameResponse;
 import com.kkinikong.be.user.service.UserService;
 import com.kkinikong.be.user.utils.CustomUserDetails;
