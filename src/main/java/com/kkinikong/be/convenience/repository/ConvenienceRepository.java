dac9059ea7a510e85639b6367f94d024db802079
chaen-ing <kimce0405@naver.com>
2025-06-04 18:44:45 +0900

    [SCRUM-119] Debug : 안쓰는 메서드 삭제

--- src/main/java/com/kkinikong/be/convenience/repository/ConvenienceRepository.java
+++ src/main/java/com/kkinikong/be/convenience/repository/ConvenienceRepository.java
@@ -1,16 +1,10 @@
 package com.kkinikong.be.convenience.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.kkinikong.be.convenience.domain.ConveniencePost;
-import com.kkinikong.be.convenience.domain.type.Brand;
 
 @Repository
 public interface ConvenienceRepository
-    extends JpaRepository<ConveniencePost, Long>, ConvenienceRepositoryCustom {
-
-  Page<ConveniencePost> findAllByBrand(Brand brand, Pageable pageable);
-}
+    extends JpaRepository<ConveniencePost, Long>, ConvenienceRepositoryCustom {}
