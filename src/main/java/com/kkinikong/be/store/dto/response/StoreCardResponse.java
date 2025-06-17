68d7ea0b1c08c93c1501a07d8a6b22f6ff2851a4
chaen-ing <kimce0405@naver.com>
2025-06-18 02:52:33 +0900

    [SCRUM-181] Refact : StoreCardResponse에서 주소 포맷 개선

--- src/main/java/com/kkinikong/be/store/dto/response/StoreCardResponse.java
+++ src/main/java/com/kkinikong/be/store/dto/response/StoreCardResponse.java
@@ -1,14 +1,20 @@
 package com.kkinikong.be.store.dto.response;
 
+import java.util.Arrays;
+
 import com.kkinikong.be.store.domain.Store;
 
 public record StoreCardResponse(
     Long id, String name, String address, long viewCount, String category) {
   public static StoreCardResponse from(Store store) {
+    String[] addressParts = store.getAddress().split(" ");
+    String refinedAddress =
+        String.join(" ", Arrays.copyOfRange(addressParts, 2, addressParts.length));
+
     return new StoreCardResponse(
         store.getId(),
         store.getName(),
-        store.getAddress().split("서구 ")[1],
+        refinedAddress,
         store.getViewCount(),
         store.getCategory().getLabel());
   }
