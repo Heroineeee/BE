73b5d5c3a2ab418c07b2bb802493210fb7af6b97
erika0915 <soohee2001@gmail.com>
2025-06-18 15:07:34 +0900

    [SCRUM-184] Feat : Feedback domain 작성

---
+++ src/main/java/com/kkinikong/be/feedback/domain/Feedback.java
@@ -0,0 +1,38 @@
+package com.kkinikong.be.feedback.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import com.kkinikong.be.global.entity.BaseEntity;
+import com.kkinikong.be.user.domain.User;
+
+@Table(name = "feeabacks")
+@Entity
+@Getter
+@NoArgsConstructor
+public class Feedback extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
+
+  @Column(name = "rating", nullable = false)
+  private int rating;
+
+  @Column(name = "content", length = 6000)
+  private String content;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @Builder
+  public Feedback(int rating, String content, User user) {
+    this.rating = rating;
+    this.content = content;
+    this.user = user;
+  }
+}
