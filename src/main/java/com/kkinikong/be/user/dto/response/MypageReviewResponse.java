94897184b82032cee09a7bc8bc00a54979a36044
erika0915 <soohee2001@gmail.com>
2025-06-27 13:40:24 +0900

    [SCRUM-186] Feat : createdAt에 TimeUtil 사용

--- src/main/java/com/kkinikong/be/user/dto/response/MypageReviewResponse.java
+++ src/main/java/com/kkinikong/be/user/dto/response/MypageReviewResponse.java
@@ -1,16 +1,16 @@
 package com.kkinikong.be.user.dto.response;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import jakarta.annotation.Nullable;
 
+import com.kkinikong.be.community.util.TimeUtil;
 import com.kkinikong.be.review.domain.Review;
 import com.kkinikong.be.review.domain.type.Tag;
 
 public record MypageReviewResponse(
     Long reviewId,
-    LocalDate reviewDate,
+    String createdAt,
     int rating,
     @Nullable List<String> tags,
     @Nullable String content,
@@ -19,7 +19,7 @@ public record MypageReviewResponse(
       Review review, @Nullable List<Tag> tags, @Nullable String imageUrl) {
     return new MypageReviewResponse(
         review.getId(),
-        review.getCreatedDate().toLocalDate(),
+        TimeUtil.relativeTimeFormatter(review.getCreatedDate()),
         review.getRating(),
         tags == null ? null : tags.stream().map(Tag::getLabel).toList(),
         review.getContent(),
