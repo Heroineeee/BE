0003297ce846db81973aa1da2bbded314296e39a
chaen-ing <kimce0405@naver.com>
2025-07-01 00:28:14 +0900

    [SCRUM-190] Fix : elastic search 애노테이션 추가

--- src/main/java/com/kkinikong/be/KkinikongBeApplication.java
+++ src/main/java/com/kkinikong/be/KkinikongBeApplication.java
@@ -3,12 +3,15 @@ package com.kkinikong.be;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
 @EnableJpaAuditing
 @EnableScheduling
+@EnableElasticsearchRepositories(
+    basePackages = "com.kkinikong.be.community.repository.elasticsearch")
 @SpringBootApplication
 public class KkinikongBeApplication {
 
