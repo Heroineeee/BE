bf36d134d739c2fde563515b3ffe81e9b6e5c4fd
erika0915 <soohee2001@gmail.com>
2025-07-04 03:15:27 +0900

    [SCRUM-245] Refactor : enum에 불필요한 부분 삭제

--- src/main/java/com/kkinikong/be/notification/domain/type/NotificationType.java
+++ src/main/java/com/kkinikong/be/notification/domain/type/NotificationType.java
@@ -4,17 +4,15 @@ import lombok.Getter;
 
 @Getter
 public enum NotificationType {
-  COMMUNITY_LIKE("게시글 좋아요", "게시글에 좋아요를 남겼어요"),
-  COMMUNITY_COMMENT("게시글 댓글", "게시글에 댓글을 남겼어요"),
-  COMMENT_LIKE("댓글 좋아요", "댓글에 좋아요를 남겼어요"),
-  COMMENT_COMMENT("댓글에 답글", "댓글에 답글을 남겼어요"),
-  CONVENIENCE_CORRECT_INFO("올바른 정보예요", "올바른 정보예요를 눌렀어요");
+  COMMUNITY_LIKE("게시글 좋아요"),
+  COMMUNITY_COMMENT("게시글 댓글"),
+  COMMENT_LIKE("댓글 좋아요"),
+  COMMENT_COMMENT("댓글에 답글"),
+  CONVENIENCE_CORRECT_INFO("올바른 정보예요");
 
   private final String label;
-  private final String defaultMessage;
 
-  NotificationType(String label, String defaultMessage) {
+  NotificationType(String label) {
     this.label = label;
-    this.defaultMessage = defaultMessage;
   }
 }
