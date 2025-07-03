a3bd40024095f5eedfa02e7dbd50095d125c9de4
erika0915 <soohee2001@gmail.com>
2025-07-03 17:11:41 +0900

    [SCRUM-245] Feat : repository 작성

---
+++ src/main/java/com/kkinikong/be/notification/repository/NotificationRepository.java
@@ -0,0 +1,7 @@
+package com.kkinikong.be.notification.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kkinikong.be.notification.domain.Notification;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {}
