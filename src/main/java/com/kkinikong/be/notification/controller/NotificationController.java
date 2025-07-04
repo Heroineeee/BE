56342bd5c0d16f3d6aac4be729c8cf8c8476cf5a
erika0915 <soohee2001@gmail.com>
2025-07-04 22:54:30 +0900

    [SCRUM-245] Fix : SSE subscribe API에 응답 타입 명시를 위한 produces 설정 추가

--- src/main/java/com/kkinikong/be/notification/controller/NotificationController.java
+++ src/main/java/com/kkinikong/be/notification/controller/NotificationController.java
@@ -1,6 +1,7 @@
 package com.kkinikong.be.notification.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -30,7 +31,7 @@ public class NotificationController {
           """
   - 클라이언트에서 서버와의 SSE 연결을 맺어 실시간으로 알림을 수신합니다.
   - 연결이 끊겼을 경우 `Last-Event-ID`를 이용해 수신하지 못한 알림을 이어받을 수 있습니다.""")
-  @GetMapping(value = "/subscribe")
+  @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
   public ResponseEntity<SseEmitter> subscribe(
       @AuthenticationPrincipal CustomUserDetails userDetails,
       @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "")
