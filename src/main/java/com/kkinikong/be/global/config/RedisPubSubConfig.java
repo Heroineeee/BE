aec4387be16f35f803e373f8f428c8b0284c8e85
erika0915 <soohee2001@gmail.com>
2025-07-04 20:34:50 +0900

    [SCRUM-245] Feat : redis pub/sub 관련 config 파일 작성

---
+++ src/main/java/com/kkinikong/be/global/config/RedisPubSubConfig.java
@@ -0,0 +1,30 @@
+package com.kkinikong.be.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+@Configuration
+public class RedisPubSubConfig {
+  public static final String NOTIFICATION_CHANNEL = "notificationChannel";
+
+  // Redis 채널 토픽 등록
+  @Bean
+  public ChannelTopic notificationChannel() {
+    return new ChannelTopic(NOTIFICATION_CHANNEL);
+  }
+
+  // Redis Pub/Sub 수신 컨테이너
+  @Bean
+  public RedisMessageListenerContainer redisMessageListenerContainer(
+      RedisConnectionFactory connectionFactory,
+      NotificationSubscriber subscriber,
+      ChannelTopic notificationChannel) {
+    RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+    container.setConnectionFactory(connectionFactory);
+    container.addMessageListener(subscriber, notificationChannel);
+    return container;
+  }
+}
