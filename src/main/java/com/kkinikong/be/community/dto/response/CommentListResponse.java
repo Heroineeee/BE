1851ecde51a58692ae92fd19cbccfde1bd9d0cb8
chaen-ing <kimce0405@naver.com>
2025-07-04 15:49:28 +0900

    [SCRUM-205] Feat: 댓글 삭제 로직 추가

--- src/main/java/com/kkinikong/be/community/dto/response/CommentListResponse.java
+++ src/main/java/com/kkinikong/be/community/dto/response/CommentListResponse.java
@@ -2,10 +2,13 @@ package com.kkinikong.be.community.dto.response;
 
 import java.util.List;
 
+import lombok.Builder;
+
 import com.kkinikong.be.community.domain.Comment;
 import com.kkinikong.be.community.util.TimeUtil;
 import com.kkinikong.be.user.utils.UserNicknameUtil;
 
+@Builder
 public record CommentListResponse(
     Long commentId,
     String content,
@@ -24,17 +27,18 @@ public record CommentListResponse(
       Boolean isLiked,
       Boolean isMyComment,
       List<CommentListResponse> replyListResponse) {
-    return new CommentListResponse(
-        comment.getId(),
-        comment.getContent(),
-        UserNicknameUtil.displayNickname(comment.getUser()),
-        TimeUtil.relativeTimeFormatter(comment.getCreatedDate()),
-        comment.isDeleted(),
-        comment.isModified(),
-        comment.getLikeCount(),
-        isLiked,
-        comment.isAuthor(),
-        isMyComment,
-        replyListResponse);
+    return CommentListResponse.builder()
+        .commentId(comment.getId())
+        .content(comment.getContent())
+        .nickname(UserNicknameUtil.displayNickname(comment.getUser()))
+        .createdAt(TimeUtil.relativeTimeFormatter(comment.getCreatedDate()))
+        .isModified(comment.isModified())
+        .isDeleted(comment.isDeleted())
+        .likeCount(comment.getLikeCount())
+        .isLiked(isLiked)
+        .isAuthor(comment.isAuthor())
+        .isMyComment(isMyComment)
+        .replyListResponse(replyListResponse)
+        .build();
   }
 }
