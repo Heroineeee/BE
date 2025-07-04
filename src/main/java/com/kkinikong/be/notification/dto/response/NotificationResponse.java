24491e8810fafa0a43e96aa66db17abdb1d2ba5c
erika0915 <soohee2001@gmail.com>
2025-07-04 16:59:54 +0900

    [SCRUM-245] Feat : 응답에 TimeUtil 활용하여 시간 추가

--- src/main/java/com/kkinikong/be/notification/dto/response/NotificationResponse.java
+++ src/main/java/com/kkinikong/be/notification/dto/response/NotificationResponse.java
@@ -1,16 +1,23 @@
 package com.kkinikong.be.notification.dto.response;
 
+import com.kkinikong.be.community.util.TimeUtil;
 import com.kkinikong.be.notification.domain.Notification;
 import com.kkinikong.be.notification.domain.type.NotificationType;
 
 public record NotificationResponse(
-    Long notificationId, NotificationType type, String content, String redirectUrl, Long targetId) {
+    Long notificationId,
+    NotificationType type,
+    String content,
+    String redirectUrl,
+    Long targetId,
+    String createdAt) {
   public static NotificationResponse from(Notification notification) {
     return new NotificationResponse(
         notification.getId(),
         notification.getNotificationType(),
         notification.getContent(),
         notification.getRedirectUrl(),
-        notification.getTargetId());
+        notification.getTargetId(),
+        TimeUtil.relativeTimeFormatter(notification.getCreatedDate()));
   }
 }
