68cc82ff58624d1ca848dfde71f2e517ad0f8844
erika0915 <soohee2001@gmail.com>
2025-07-04 21:16:24 +0900

    [SCRUM-245] Feat : NotificationListener에서 Redis Pub/Sub 기반 알림 발행 적용

--- src/main/java/com/kkinikong/be/notification/event/NotificationEventListener.java
+++ src/main/java/com/kkinikong/be/notification/event/NotificationEventListener.java
@@ -5,23 +5,27 @@ import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 
-import com.kkinikong.be.notification.infrastructure.sse.SseNotificationService;
+import com.kkinikong.be.notification.dto.NotificationMessage;
+import com.kkinikong.be.notification.infrastructure.pubsub.NotificationPublisher;
 import com.kkinikong.be.notification.util.NotificationFactory;
 
 @Component
 @RequiredArgsConstructor
 public class NotificationEventListener {
 
-  private final SseNotificationService notificationService;
+  private final NotificationPublisher notificationPublisher;
   private final NotificationFactory notificationFactory;
 
   @EventListener
   public void handleNotificationEvent(NotificationEvent event) {
-    String content =
-        notificationFactory.createContent(
-            event.getType(), event.getSenderNickname(), event.getTarget());
-
-    notificationService.send(
-        event.getReceiver(), event.getType(), content, event.getTargetId(), event.getRedirectUrl());
+    NotificationMessage message =
+        new NotificationMessage(
+            event.getReceiver().getId(),
+            event.getType(),
+            notificationFactory.createContent(
+                event.getType(), event.getSenderNickname(), event.getTarget()),
+            event.getRedirectUrl(),
+            event.getTargetId());
+    notificationPublisher.publish(message);
   }
 }
