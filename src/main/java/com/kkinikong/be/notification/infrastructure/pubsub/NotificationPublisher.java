121ee944b063459c89b6cd511cdec1c4b6f274e2
erika0915 <soohee2001@gmail.com>
2025-07-06 23:33:53 +0900

    [SCRUM-287] Fix : 알림 직렬화 설정 추가

--- src/main/java/com/kkinikong/be/notification/infrastructure/pubsub/NotificationPublisher.java
+++ src/main/java/com/kkinikong/be/notification/infrastructure/pubsub/NotificationPublisher.java
@@ -4,6 +4,8 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 
 import com.kkinikong.be.notification.dto.NotificationMessage;
@@ -13,8 +15,14 @@ import com.kkinikong.be.notification.dto.NotificationMessage;
 public class NotificationPublisher {
   private final RedisTemplate<String, Object> pubSubRedisTemplate;
   private final ChannelTopic channelTopic;
+  private final ObjectMapper objectMapper;
 
   public void publish(NotificationMessage message) {
-    pubSubRedisTemplate.convertAndSend(channelTopic.getTopic(), message);
+    try {
+      String json = objectMapper.writeValueAsString(message);
+      pubSubRedisTemplate.convertAndSend(channelTopic.getTopic(), json);
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException("알림 직렬화 실패", e);
+    }
   }
 }
