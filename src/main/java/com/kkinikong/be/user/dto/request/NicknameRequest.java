c068d4dcc7ef8a2179541880057d07ef0e3322be
erika0915 <soohee2001@gmail.com>
2025-07-07 20:53:59 +0900

    [SCRUM-287] Refactor : 닉네임 공백 포함 최대 7자로 수정

--- src/main/java/com/kkinikong/be/user/dto/request/NicknameRequest.java
+++ src/main/java/com/kkinikong/be/user/dto/request/NicknameRequest.java
@@ -4,6 +4,5 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record NicknameRequest(
-    @NotBlank(message = "닉네임은 필수 입력입니다.")
-        @Size(max = 10, message = "닉네임은 공백 포함 최대 10자까지 입력할 수 있습니다.")
+    @NotBlank(message = "닉네임은 필수 입력입니다.") @Size(max = 7, message = "닉네임은 공백 포함 최대 7자까지 입력할 수 있습니다.")
         String nickname) {}
