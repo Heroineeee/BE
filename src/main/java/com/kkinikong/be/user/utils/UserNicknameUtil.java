7dbb9e6ce90c8ee1e2b0112ac10997d72e344a27
erika0915 <soohee2001@gmail.com>
2025-07-07 20:57:49 +0900

    [SCRUM-287] Refactor : '탈퇴한 회원'으로 변경

--- src/main/java/com/kkinikong/be/user/utils/UserNicknameUtil.java
+++ src/main/java/com/kkinikong/be/user/utils/UserNicknameUtil.java
@@ -4,7 +4,7 @@ import com.kkinikong.be.user.domain.User;
 
 public class UserNicknameUtil {
   public static String displayNickname(User user) {
-    return user.isDeleted() ? "탈퇴한 회원입니다" : user.getNickname();
+    return user.isDeleted() ? "탈퇴한 회원" : user.getNickname();
   }
 
   public static String checkReportNickname(User user) {
