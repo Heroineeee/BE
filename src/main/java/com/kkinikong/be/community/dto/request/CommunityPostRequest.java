3b4e8981b0011bc0227843bc3ba07384df75c28d
chaen-ing <kimce0405@naver.com>
2025-07-08 21:20:23 +0900

    [SCRUM-150] Refactor: @notnull 제거

--- src/main/java/com/kkinikong/be/community/dto/request/CommunityPostRequest.java
+++ src/main/java/com/kkinikong/be/community/dto/request/CommunityPostRequest.java
@@ -7,5 +7,5 @@ import com.kkinikong.be.community.domain.type.Category;
 
 public record CommunityPostRequest(
     @NotNull @Size(min = 1, max = 25) String title,
-    @NotNull @Size(max = 5000) String content,
+    @Size(max = 5000) String content,
     @NotNull Category category) {}
--- src/main/java/com/kkinikong/be/community/dto/request/CommunityPostUpdateRequest.java
+++ src/main/java/com/kkinikong/be/community/dto/request/CommunityPostUpdateRequest.java
@@ -9,6 +9,6 @@ import com.kkinikong.be.community.domain.type.Category;
 
 public record CommunityPostUpdateRequest(
     @NotNull @Size(min = 1, max = 25) String title,
-    @NotNull @Size(max = 5000) String content,
+    @Size(max = 5000) String content,
     @NotNull Category category,
     List<String> remainingImageUrls) {}
