a6f992b4b1666672c85beebf41dfafc0bf2a4193
erika0915 <soohee2001@gmail.com>
2025-07-10 18:02:27 +0900

    [SCRUM-245] Chore : 불필요한 필드 삭제

--- src/main/java/com/kkinikong/be/notification/infrastructure/pubsub/NotificationPublisher.java
+++ src/main/java/com/kkinikong/be/notification/infrastructure/pubsub/NotificationPublisher.java
@@ -4,7 +4,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,7 +15,6 @@ import com.kkinikong.be.notification.dto.NotificationMessage;
 public class NotificationPublisher {
   private final RedisTemplate<String, Object> pubSubRedisTemplate;
   private final ChannelTopic channelTopic;
-  private final ObjectMapper objectMapper;
 
   public void publish(NotificationMessage message) {
     pubSubRedisTemplate.convertAndSend(channelTopic.getTopic(), message);
