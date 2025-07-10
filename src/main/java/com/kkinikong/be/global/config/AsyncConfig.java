7ce42e1022b03a10df67f90da098777c290ca586
erika0915 <soohee2001@gmail.com>
2025-07-10 17:34:38 +0900

    [SCRUM-245] Feat : 비동기 설정 파일 (AsyncConfig) 작성

---
+++ src/main/java/com/kkinikong/be/global/config/AsyncConfig.java
@@ -0,0 +1,25 @@
+package com.kkinikong.be.global.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync // 프로젝트 전체에서 @Async 어노테이션을 사용할 수 있도록 함
+public class AsyncConfig implements AsyncConfigurer {
+
+  @Bean(name = "notificationExecutor")
+  public Executor asyncExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(5); // 기본 스레드 수
+    executor.setMaxPoolSize(10); // 최대 스레드 수
+    executor.setQueueCapacity(100); // 대기 큐
+    executor.setThreadNamePrefix("NotificationExecutor-");
+    executor.initialize();
+    return executor;
+  }
+}
