2b775fa694dc32c84d2c121f663bc1ad77db5243
erika0915 <soohee2001@gmail.com>
2025-07-27 11:43:20 +0900

    [SCRUM-338] Feat : csv파일 db에 넣을 때 location 추가

--- src/main/java/com/kkinikong/be/store/repository/storeupload/StoreJdbcRepositoryImpl.java
+++ src/main/java/com/kkinikong/be/store/repository/storeupload/StoreJdbcRepositoryImpl.java
@@ -27,8 +27,8 @@ public class StoreJdbcRepositoryImpl implements StoreJdbcRepository {
   public void saveAllByJdbcTemplate(List<Store> stores) {
     String sql =
         "INSERT INTO stores "
-            + "(name, region, category, address, latitude, longitude, rating_avg, scrap_count, review_count, view_count, updated_date, created_date, modified_date) "
-            + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            + "(name, region, category, address, latitude, longitude, location, rating_avg, scrap_count, review_count, view_count, updated_date, created_date, modified_date) "
+            + "VALUES (?, ?, ?, ?, ?, ?, ST_GeomFromText(? , 4326), ?, ?, ?, ?, ?, ?, ?)";
 
     jdbcTemplate.batchUpdate(
         sql,
@@ -41,13 +41,15 @@ public class StoreJdbcRepositoryImpl implements StoreJdbcRepository {
           ps.setString(4, store.getAddress());
           ps.setDouble(5, store.getLatitude());
           ps.setDouble(6, store.getLongitude());
-          ps.setDouble(7, 0.0);
-          ps.setLong(8, 0L);
+          ps.setString(7, String.format("POINT(%f %f)", store.getLatitude(), store.getLongitude()));
+
+          ps.setDouble(8, 0.0);
           ps.setLong(9, 0L);
           ps.setLong(10, 0L);
-          ps.setObject(11, store.getUpdatedDate());
-          ps.setObject(12, LocalDate.now());
+          ps.setLong(11, 0L);
+          ps.setObject(12, store.getUpdatedDate());
           ps.setObject(13, LocalDate.now());
+          ps.setObject(14, LocalDate.now());
         });
   }
 
