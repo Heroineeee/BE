caf7d29ccd52ff2dd233210d6f394964c910673d
erika0915 <soohee2001@gmail.com>
2025-08-13 16:25:24 +0900

    [SCRUM-355] Feat: 로컬 환경 로깅 용 Interceptor 생성

---
+++ src/main/java/com/kkinikong/be/global/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.kkinikong.be.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.RequiredArgsConstructor;
+
+import com.kkinikong.be.global.logging.LoggingInterceptor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+  private final LoggingInterceptor loggingInterceptor;
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry
+        .addInterceptor(loggingInterceptor)
+        .addPathPatterns("/**")
+        .excludePathPatterns("/swagger-ui/**", "/v3/api-docs/**", "/actuator/**");
+  }
+}
