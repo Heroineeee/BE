1b86fd194fb503e355e153b5d7ee3a936327a37d
erika0915 <soohee2001@gmail.com>
2025-08-13 17:00:58 +0900

    [SCRUM-355] Feat: aop를 활용한 로깅 구현

---
+++ src/main/java/com/kkinikong/be/global/logging/LoggingAspect.java
@@ -0,0 +1,36 @@
+package com.kkinikong.be.global.logging;
+
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+@Slf4j
+@Component
+public class LoggingAspect {
+
+  @Around("execution(* com.kkinikong.be..*Service.*(..))")
+  public Object logServiceExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+    long start = System.currentTimeMillis();
+    Object result = joinPoint.proceed();
+    long end = System.currentTimeMillis();
+    long duration = end - start;
+    String methodName = joinPoint.getSignature().toShortString();
+    log.info("⏱️ 비즈니스 로직: {} 실행 시간 {}ms", methodName, duration);
+    return result;
+  }
+
+  @AfterReturning(
+      pointcut = "execution(* com.kkinikong.be..*Repository.*(..))",
+      returning = "result")
+  public void logRepositoryExecution(JoinPoint joinPoint, Object result) {
+    String methodName = joinPoint.getSignature().toShortString();
+    String resultType = (result != null) ? result.getClass().getSimpleName() : "void";
+    log.info("💾 데이터 접근: {} 성공 | 반환 값 타입: {}", methodName, resultType);
+  }
+}
