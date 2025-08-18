5370340147b3e4e506bd5cf42ad87de697fbdaa9
chaen-ing <kimce0405@naver.com>
2025-08-19 00:30:23 +0900

    [SCRUM-364] Rename: 변수명 수정

--- src/main/java/com/kkinikong/be/convenience/dto/response/ConveniencePostListResponse.java
+++ src/main/java/com/kkinikong/be/convenience/dto/response/ConveniencePostListResponse.java
@@ -4,7 +4,7 @@ import com.kkinikong.be.convenience.domain.ConveniencePost;
 import com.kkinikong.be.global.util.TimeUtil;
 
 public record ConveniencePostListResponse(
-    Long id, String name, boolean isAvailable, String CreatedAt) {
+    Long id, String name, boolean isAvailable, String relativeCreatedAt) {
   public static ConveniencePostListResponse from(ConveniencePost conveniencePost) {
     return new ConveniencePostListResponse(
         conveniencePost.getId(),
