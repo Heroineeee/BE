14a7c02f07938f14fd9afaa03939b9a9bb01a9e7
chaen-ing <kimce0405@naver.com>
2025-10-22 23:23:55 +0900

    [SCRUM-415] Refactor : FeedbackType 변경 및 FeedbackRequest 수정

--- src/main/java/com/kkinikong/be/feedback/dto/request/FeedbackRequest.java
+++ src/main/java/com/kkinikong/be/feedback/dto/request/FeedbackRequest.java
@@ -1,5 +1,7 @@
 package com.kkinikong.be.feedback.dto.request;
 
+import java.util.List;
+
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
@@ -7,4 +9,4 @@ import jakarta.validation.constraints.Size;
 import com.kkinikong.be.feedback.domain.type.FeedbackType;
 
 public record FeedbackRequest(
-    @Min(1) @Max(5) int rating, @Size(max = 6000) String content, FeedbackType type) {}
+    @Min(1) @Max(5) int rating, @Size(max = 6000) String content, List<FeedbackType> type) {}
