686a4e05155fad9ffe7b118ee9d7707a5493eddf
erika0915 <soohee2001@gmail.com>
2026-01-08 00:15:49 +0900

    [SCRUM-433] Feat: 디스코드 설정 추가

--- src/main/java/com/kkinikong/be/feedback/event/FeedbackEventListener.java
+++ src/main/java/com/kkinikong/be/feedback/event/FeedbackEventListener.java
@@ -8,12 +8,17 @@ import org.springframework.transaction.event.TransactionalEventListener;
 import lombok.RequiredArgsConstructor;
 
 import com.kkinikong.be.feedback.event.payload.FeedbackCreatedEvent;
+import com.kkinikong.be.feedback.util.discord.DiscordClient;
 
 @Component
 @RequiredArgsConstructor
 public class FeedbackEventListener {
 
+  private final DiscordClient discordClient;
+
   @Async("externalApiExecutor")
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-  public void handleFeedbackCreatedEvent(FeedbackCreatedEvent feedbackCreatedEvent) {}
+  public void handleFeedbackCreatedEvent(FeedbackCreatedEvent feedbackCreatedEvent) {
+    discordClient.sendFeedbackAlert(feedbackCreatedEvent);
+  }
 }
